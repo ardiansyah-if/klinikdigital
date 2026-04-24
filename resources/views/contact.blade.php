0e13e84d272216e023c50b5809eab19a8691878d
Ardiansyah <ardiansyah@MacBook-Air-Ardiansyah.local>
2026-04-24 15:12:22 +0700

    views

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <!-- When there is no desire, all things are at peace. - Laozi -->
+     Silahkan menghubungi nomor yang tertera untuk melakukan pembookingan jadwal konsultasi: 083164332970 
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
+     Selamat Datang di Klinik Digital Polibatam {{$nama}}<br>
+     Anda adalah seorang {{$pekerjaan}}
+</div>
